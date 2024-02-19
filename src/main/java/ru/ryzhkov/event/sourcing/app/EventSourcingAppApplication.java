e8733b72335b96cf799bbb12d70caea7822163c4
sryzhkov <spryzhkov@sdvor.com>
2024-02-19 08:08:30 +0300

    initialize project and config

---
+++ src/main/java/ru/ryzhkov/event/sourcing/app/EventSourcingAppApplication.java
@@ -0,0 +1,13 @@
+package ru.ryzhkov.event.sourcing.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventSourcingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventSourcingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/ryzhkov/event/sourcing/app/EventSourcingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ryzhkov.event.sourcing.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventSourcingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
